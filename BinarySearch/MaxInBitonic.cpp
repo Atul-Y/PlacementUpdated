cfeff7be30ffcbf64454fe64b9313da45668b9fb
ATUL YADAV <yadavatul95@gmail.com>
2025-10-04 19:39:06 +0530

    changes donee

---
+++ BinarySearch/MaxInBitonic.cpp
@@ -0,0 +1,10 @@
+//difference between monotonic and bitonic array is that in monotonic array elements are either increasing or decreasing
+//in bitonic array elements are first increasing and then decreasing
+//for example: 1 3 8 12 4 2 is a bitonic array
+//we have to find the maximum element in the bitonic array
+
+
+//Maximum in Bitonic array = peak element in the array (greater than its adjacent elements) aldready solved in PeakInUnsorted.cpp
+
+//Note in bitonic array first half is increasing and second half is decreasing whereas in peak element array no such condition is there
+//also in bitonic array there is only one peak element whereas in peak element array there can be multiple peak elements
